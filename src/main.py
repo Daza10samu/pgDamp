7a9fb8ab36a064cc9356f27c775dd0b4f8b5faf3
Daza10samu <andreyhigher@gmail.com>
2021-10-17 16:49:31 +0300

    [fix] fix config loading, connecting to DB and sending file. And add timeout

--- src/main.py
+++ src/main.py
@@ -5,16 +5,17 @@ from yaml import safe_load
 
 from telebot import TeleBot
 
-with Path("config.yaml").open() as stream:
+with Path("config.yml").open() as stream:
     config = safe_load(stream)
 
 bot = TeleBot(config["main"]["bot_token"])
 
 system(
-    f"PGPASSWORD={config['main']['db_pass']} pg_dump -U {config['main']['db_user']}"
+    f"PGPASSWORD={config['main']['db_pass']} PGPORT={config['main']['db_port']} PGHOSTADDR={config['main']['db_host']}"
+    f" pg_dump -U {config['main']['db_user']}"
     f"  --dbname=postgres --file='dump.sql' --create"
 )
 
 for chat in config["main"]["chats"]:
-    with Path('dump.sql').open() as file:
-        bot.send_document(chat, file)
+    with Path('dump.sql').open("rb") as file:
+        bot.send_document(chat, file, timeout=config["main"]["timeout"])
